c928e11d6e878f57531f850111f116576ccff8a6
doksanpz <114029449+doksanpz@users.noreply.github.com>
2023-01-26 13:47:42 +0200

    Add tasks for setsAndMaps
    Problem solutions for setsAndMaps

---
+++ setsAndMaps/P04countRealNumbers.java
@@ -0,0 +1,29 @@
+package setsAndMaps;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class P04countRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<Double, Integer> occurrences = new LinkedHashMap<>();
+
+        String[] numbers = scanner.nextLine().split("\\s+");
+
+        for (String number : numbers) {
+            double current = Double.parseDouble(number);
+
+            if (occurrences.containsKey(current)) {
+                Integer count = occurrences.get(current);
+                occurrences.put(current, count + 1);
+            } else {
+                occurrences.put(current, 1);
+            }
+        }
+        for (Double currentNumber : occurrences.keySet()) {
+            System.out.printf("%.1f -> %d%n", currentNumber, occurrences.get(currentNumber));
+        }
+    }
+}
---
+++ setsAndMaps/P08academyGraduation.java
@@ -0,0 +1,34 @@
+package setsAndMaps;
+
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class P08academyGraduation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Map<String, List<Double>> averageGradeByStudents = new TreeMap<>();
+
+        while (--n >= 0) {
+            String name = scanner.nextLine();
+            double[] grades = Arrays
+                    .stream(scanner.nextLine().split(" "))
+                    .mapToDouble(Double::parseDouble)
+                    .toArray();
+
+            averageGradeByStudents.putIfAbsent(name, new ArrayList<>());
+
+            for (double grade : grades) {
+                averageGradeByStudents.get(name).add(grade);
+            }
+
+        }
+        averageGradeByStudents.forEach((key, value) -> {
+            double sumOfGrades = 0;
+            for (Double grade : value) {
+                sumOfGrades += grade;
+            }
+            System.out.printf("%s is graduated with %s%n", key, sumOfGrades / value.size());
+        });
+    }
+}
